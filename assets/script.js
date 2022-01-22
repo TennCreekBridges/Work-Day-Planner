dceb1ca8c5dc5698f1e4ef7f9e8375567e637f76
TennCreekBridges <bridgettvalet@gmail.com>
2022-01-22 16:02:53 -0600

    adds remaining time functions

--- assets/script.js
+++ assets/script.js
@@ -146,3 +146,129 @@ function elevenAm() {
         output3.textContent = input3.value;
     }
 }
+function twelvePm() {
+    // set variables for input, output and local storage
+    let input4 = document.querySelector("#time12pm");
+    let output4 = document.querySelector("#time12pm");
+    let save4 = document.querySelector("#time12pm");
+    // listen for click on save button and execute function updateOutput4
+    save4.addEventListener("click", updateOutput4);
+    // looks for existing local storage content from user input and adds if exists
+    output4.textContent = localStorage.getItem("content4");
+    input4.value = localStorage.getItem("content4");
+    // sets key value pair and defines content4
+    function updateOutput4() {
+        let content4 = "";
+        localStorage.setItem("content4", input4.value);
+        // displays old and new content in time block
+        output4.textContent = input4.value;
+    }
+}
+function twelvePm() {
+    // set variables for input, output and local storage
+    let input4 = document.querySelector("#time12pm");
+    let output4 = document.querySelector("#time12pm");
+    let save4 = document.querySelector("#time12pm");
+    // listen for click on save button and execute function updateOutput4
+    save4.addEventListener("click", updateOutput4);
+    // looks for existing local storage content from user input and adds if exists
+    output4.textContent = localStorage.getItem("content4");
+    input4.value = localStorage.getItem("content4");
+    // sets key value pair and defines content4
+    function updateOutput4() {
+        let content4 = "";
+        localStorage.setItem("content4", input4.value);
+        // displays old and new content in time block
+        output4.textContent = input4.value;
+    }
+}
+function onePm() {
+    // set variables for input, output and local storage
+    let input5 = document.querySelector("#time1pm");
+    let output5 = document.querySelector("#time1pm");
+    let save5 = document.querySelector("#time1pm");
+    // listen for click on save button and execute function updateOutput5
+    save5.addEventListener("click", updateOutput5);
+    // looks for existing local storage content from user input and adds if exists
+    output5.textContent = localStorage.getItem("content5");
+    input5.value = localStorage.getItem("content5");
+    // sets key value pair and defines content5
+    function updateOutput5() {
+        let content5 = "";
+        localStorage.setItem("content5", input5.value);
+        // displays old and new content in time block
+        output5.textContent = input5.value;
+    }
+}
+function twoPm() {
+    // set variables for input, output and local storage
+    let input6 = document.querySelector("#time2pm");
+    let output6 = document.querySelector("#time2pm");
+    let save6 = document.querySelector("#time2pm");
+    // listen for click on save button and execute function updateOutput6
+    save6.addEventListener("click", updateOutput6);
+    // looks for existing local storage content from user input and adds if exists
+    output6.textContent = localStorage.getItem("content6");
+    input6.value = localStorage.getItem("content6");
+    // sets key value pair and defines content6
+    function updateOutput6() {
+        let content6 = "";
+        localStorage.setItem("content6", input6.value);
+        // displays old and new content in time block
+        output6.textContent = input6.value;
+    }
+}
+function threePm() {
+    // set variables for input, output and local storage
+    let input7 = document.querySelector("#time3pm");
+    let output7 = document.querySelector("#time3pm");
+    let save7 = document.querySelector("#time3pm");
+    // listen for click on save button and execute function updateOutput7
+    save7.addEventListener("click", updateOutput7);
+    // looks for existing local storage content from user input and adds if exists
+    output7.textContent = localStorage.getItem("content7");
+    input7.value = localStorage.getItem("content7");
+    // sets key value pair and defines content7
+    function updateOutput7() {
+        let content7 = "";
+        localStorage.setItem("content7", input7.value);
+        // displays old and new content in time block
+        output7.textContent = input7.value;
+    }
+}
+function fourPm() {
+    // set variables for input, output and local storage
+    let input8 = document.querySelector("#time4pm");
+    let output8 = document.querySelector("#time4pm");
+    let save8 = document.querySelector("#time4pm");
+    // listen for click on save button and execute function updateOutput8
+    save8.addEventListener("click", updateOutput8);
+    // looks for existing local storage content from user input and adds if exists
+    output8.textContent = localStorage.getItem("content8");
+    input8.value = localStorage.getItem("content8");
+    // sets key value pair and defines content8
+    function updateOutput8() {
+        let content8 = "";
+        localStorage.setItem("content8", input8.value);
+        // displays old and new content in time block
+        output8.textContent = input8.value;
+    }
+}
+function fivePm() {
+    // set variables for input, output and local storage
+    let input9 = document.querySelector("#time5pm");
+    let output9 = document.querySelector("#time5pm");
+    let save9 = document.querySelector("#time5pm");
+    // listen for click on save button and execute function updateOutput9
+    save9.addEventListener("click", updateOutput9);
+    // looks for existing local storage content from user input and adds if exists
+    output9.textContent = localStorage.getItem("content9");
+    input9.value = localStorage.getItem("content9");
+    // sets key value pair and defines content9
+    function updateOutput9() {
+        let content9 = "";
+        localStorage.setItem("content9", input9.value);
+        // displays old and new content in time block
+        output9.textContent = input9.value;
+    }
+}
